709f0dd8bb98afab8ed1e35f840042d0925344c4
TheoLong <TheoLoong@gmail.com>
2015-11-14 23:41:37 -0500

    Update debug.py

--- sabertooth/script/debug.py
+++ sabertooth/script/debug.py
@@ -4,12 +4,16 @@ from control_motor import moveRight
 
 left,right=raw_input("command: ").split()
 left, right = [int(left), int(right)]
+print(left)
+print(right)
 i=0
 while (i != 100):
 	moveLeft(left)
 	moveRight(right)
 	left,right=raw_input("command: ").split()
 	left, right = [int(left), int(right)]
+	print(left)
+	print(right)
 	i=i+1
 	
 

232b134911fbcfb1e68c0e175288f192edeb541c
TheoLong <TheoLoong@gmail.com>
2015-11-15 00:32:47 -0500

    Update control_motor.py
    add speed cap

--- sabertooth/script/control_motor.py
+++ sabertooth/script/control_motor.py
@@ -13,16 +13,23 @@ def moveLeft(power):
 	if power==0:
 		output=64
 	elif power>0:
+		if power>100:
+			power=100
+			print("speed cap")
 		output=(power/float(100))*step
 		output=round(output,0)
 		output=int(output)
 		output=64+output
 	elif power<0:
+		if power<-100:
+			power=-100
+			print("speed cap")
 		power=abs(power)
 		output=(power/float(100))*step	
 		output=round(output,0)
 		output=int(output)
 		output=64-output
+			
 	else:
 		output=0
 		print("power input error")
@@ -33,11 +40,17 @@ def moveRight(power):
 	if power==0:
 		output=192
 	elif power>0:
+		if power>100:
+			power=100
+			print("speed cap")
 		output=(power/float(100))*step	
 		output=round(output,0)
 		output=int(output)
 		output=192+output
 	elif power<0:
+		if power<-100:
+			power=-100
+			print("speed cap")
 		power=abs(power)
 		output=(power/float(100))*64	
 		output=round(output,0)

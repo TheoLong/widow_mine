f6cdc30d903e6b8cd213203f395554ac5f7fc636
Theo Long <theoloong@gmail.com>
2015-11-14 23:04:51 -0500

    add motor control

---
+++ sabertooth/script/control_motor.py
@@ -0,0 +1,44 @@
+import Adafruit_BBIO.UART as UART
+import serial
+import math
+UART.setup("UART1")
+ser = serial.Serial(port = "/dev/ttyo0",baudrate = 9600)
+serial.close()
+serial.open()
+step=63
+def moveLeft(power):
+	if power==0:
+		output=0
+	elif power>0:
+		output=(power/100)step	
+		round(output,0)
+		output=64+output
+	elif power<0:
+		abs(power)
+		output=(power/100)step	
+		round(output,0)
+	else:
+		output=0
+		print("power input error")
+	ser.write(output)
+
+def moveRight(power):
+	if power==0:
+		output=192
+	elif power>0:
+		output=(power/100)step	
+		round(output,0)
+		output=192+output
+	elif power<0:
+		abs(power)
+		output=(power/100)step	
+		round(output,0)
+		output=127+output
+	else:
+		output=0
+		print("power input error")
+	ser.write(output)
+
+
+
+	
---
+++ sabertooth/script/debug.py
@@ -0,0 +1,10 @@
+import control_motor
+
+
+left,right=raw_input("command: ").split()
+while (isinstance(left,int) and isinstance(right,int))
+	moveLeft(left)
+	moveRight(right)
+	left,right=raw_input("command: ").split()
+	
+

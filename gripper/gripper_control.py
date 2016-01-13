946cff5e3698b2dbccb32e691f4cb77530bbd9ee
Theo Long <theoloong@gmail.com>
2016-01-13 17:21:15 -0500

    grip control file

---
+++ gripper/gripper_control.py
@@ -0,0 +1,41 @@
+
+
+def movePitch (pitch, pcurrent)
+	upper=180
+	lower=0
+	if current==upper or current== lower
+		return current
+	else
+		if pitch>=0.1:
+			pcurrent=pcurrent+step*pitch
+			pitch(pcurrent)
+		else if pitch<=0.1:
+			pcurrent=pcurrent-step*pitch
+			pitch(pcurrent)
+		return pcurrent
+def mvoeYaw (yaw,ycurrent)
+	upper=180
+	lower=0
+	if ycurrent==upper or ycurrent== lower
+		return current
+	else
+		if pitch>=0.1:
+			ycurrent=current+step*pitch
+			pitch(ycurrent)
+		else if pitch<=0.1:
+			ycurrent=ycurrent-step*pitch
+			pitch(ycurrent)
+		return ycurrent
+def grab(grab,gcurrent)
+	upper=180
+	lower=0
+	if gcurrent==upper or gcurrent== lower
+		return current
+	else
+		if pitch>=0.1:
+			gcurrent=gcurrent+step*pitch
+			pitch(gcurrent)
+		else if pitch<=0.1:
+			gcurrent=gcurrent-step*pitch
+			pitch(gcurrent)
+		return current
---
+++ sabertooth/script/differential.py
@@ -0,0 +1,18 @@
+from control_motor import moveLeft
+from control_motor import moveRight
+
+def diff_control(gas, steering):
+	#if car is not moving
+	if -0.03 <= gas <= 0.03:
+		steering_power=steering*100
+		moveLeft(steering_power)
+		moveRight(-steering_power)
+	#if car is moving
+	else
+		if steering>=0:
+		moveLeft(gas)
+		moveRight(steering*gas)
+		else
+		moveLeft(gas*abs(steering))
+		moveRight(gas)
+

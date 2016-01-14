43b355379791d1f387f9bfdbb43e119aae94c8c1
TheoLong <TheoLoong@gmail.com>
2016-01-13 21:12:47 -0500

    Update servo_drive.py

--- gripper/script/servo_drive.py
+++ gripper/script/servo_drive.py
@@ -18,6 +18,7 @@ class pwm:
         pwm_roll = 0.0
         pwm_roll = angle*0.05333+2.4
 	PWM.set_duty_cycle("P9_16", pwm_roll)
+	print pwm_roll
     def pitch(angle):
 	pwm_pitch = 0.0
         pwm_pitch = angle*0.05333+2.4

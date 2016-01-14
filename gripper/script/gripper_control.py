7a84740414e702a73f403ded6e34115e38ca6fb2
TheoLong <TheoLoong@gmail.com>
2016-01-13 21:23:29 -0500

    Update gripper_control.py

--- gripper/script/gripper_control.py
+++ gripper/script/gripper_control.py
@@ -1,44 +1,49 @@
 import servo_drive as pwm
 
 def movePitch (pitch, pcurrent):
-	step=50
-	upper=180
-	lower=0
+	step=50.0
+	upper=180.0
+	lower=0.0
 	if pcurrent==upper or pcurrent== lower:
 		return pcurrent
 	else:
 		if pitch>=0.1:
 			pcurrent=pcurrent+step*pitch
 			pwm.pitch(pcurrent)
+			return pcurrent
 		elif pitch<=-0.1:
 			pcurrent=pcurrent-step*pitch
 			pwm.pitch(pcurrent)
-		return pcurrent
+			return pcurrent
 def moveRoll (roll,rcurrent):
-	step=50
-	upper=180
-	lower=0
+	step=50.0
+	upper=180.0
+	lower=0.0
 	if rcurrent==upper or rcurrent== lower:
 		return rcurrent
 	else:
 		if roll>=0.1:
 			rcurrent=rcurrent+step*roll
 			pwm.roll(rcurrent)
+			print rcurrent
+			return rcurrent
+		
 		elif roll<=-0.1:
 			rcurrent=rcurrent-step*roll
 			pwm.roll(rcurrent)
-		return ycurrent
+		 	return rcurrent
 def grab(grab,gcurrent):
-	step=50
-	upper=180
-	lower=0
+	step=50.0
+	upper=180.0
+	lower=0.0
 	if gcurrent==upper or gcurrent== lower:
 		return gcurrent
 	else:
 		if grab>0:
 			gcurrent=gcurrent+step*grab
 			pwm.gripper(gcurrent)
+			return gcurrent
 		elif grab<0:
 			gcurrent=gcurrent-step*grab
 			pwm.gripper(gcurrent)
-		return gcurrent
+			return gcurrent

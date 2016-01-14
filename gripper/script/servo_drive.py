f01d7e35ad3b89259ece7b8a5049063add94115a
TheoLong <TheoLoong@gmail.com>
2016-01-13 20:49:30 -0500

    Update servo_drive.py

--- gripper/script/servo_drive.py
+++ gripper/script/servo_drive.py
@@ -6,23 +6,23 @@ import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
 # import adafruit pwm
 
-class pwm(self):
-    def __init__(self):
-	self.pwm_roll = pwm_roll
-	self.pwm_pitch = pwm_pitch
-	self.pwm_gripper = pwm_gripper
+class pwm:
+    def __init__():
+	pwm_roll = pwm_roll
+	pwm_pitch = pwm_pitch
+	pwm_gripper = pwm_gripper
 	PWM.start("P9_14",0,50)
 	PWM.start("P9_16",0,50)
 	PWM.start("P8_13",0,50)
-    def roll(self, angle):
+    def roll(angle):
         pwm_roll = 0.0
         pwm_roll = angle*0.05333+2.4
 	PWM.set_duty_cycle("P9_16", pwm_roll)
-    def pitch(self, angle):
+    def pitch(angle):
 	pwm_pitch = 0.0
         pwm_pitch = angle*0.05333+2.4
 	PWM.set_duty_cycle("P9_16", pwm_pitch)
-    def gripper(self, angle):
+    def gripper(angle):
         pwm_gripper = 0.0
         pwm_gripper = angle*0.05333+2.4
 	PWM.set_duty_cycle("P9_16", pwm_gripper)

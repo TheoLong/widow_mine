e5389807348dc896412ddb15eff26fcbb3a28632
Theo Long <theoloong@gmail.com>
2016-01-13 20:04:18 -0500

    add gripper package

---
+++ gripper/script/gripper_control.py
@@ -0,0 +1,44 @@
+from servo_drive import pwm as pwm
+
+def movePitch (pitch, pcurrent)
+	step=50
+	upper=180
+	lower=0
+	if current==upper or current== lower
+		return pcurrent
+	else
+		if pitch>=0.1:
+			pcurrent=pcurrent+step*pitch
+			pwm.pitch(pcurrent)
+		else if pitch<=-0.1:
+			pcurrent=pcurrent-step*pitch
+			pwm.pitch(pcurrent)
+		return pcurrent
+def moveRoll (roll,rcurrent)
+	step=50
+	upper=180
+	lower=0
+	if rcurrent==upper or rcurrent== lower
+		return rcurrent
+	else
+		if roll>=0.1:
+			rcurrent=rcurrent+step*roll
+			pwm.roll(rcurrent)
+		else if roll<=-0.1:
+			rcurrent=rcurrent-step*roll
+			pwm.roll(rcurrent)
+		return ycurrent
+def grab(grab,gcurrent)
+	step=50
+	upper=180
+	lower=0
+	if gcurrent==upper or gcurrent== lower
+		return gcurrent
+	else
+		if grab>0:
+			gcurrent=gcurrent+step*grab
+			pwm.gripper(gcurrent)
+		else if grab<0:
+			gcurrent=gcurrent-step*grab
+			pwm.gripper(gcurrent)
+		return gcurrent
